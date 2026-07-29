698423fbe2e7b268b3cdea91f488a12f986180aa
mercierd575 <mercierd575@gmail.com>
2026-07-29 16:03:59 -0400

    Added comments to the solution

--- LeetCode/46 - Permutations/permutations.py
+++ LeetCode/46 - Permutations/permutations.py
@@ -1,14 +1,15 @@
 from typing import List
 
 
+# this recursive method will return all permutations of nums
 def permute(nums: List[int]) -> List[List[int]]:
     answer: List[List[int]] = []
-    if len(nums) <= 1:
+    if len(nums) <= 1:                              # base case
         return [nums[:]]
-    for k in range(len(nums)):
-        l: List[int] = [nums[k]]
-        w: List[int] = nums[:k] + nums[k + 1:]
-        for p in permute(w):
-            answer.append(l + p)
+    for k in range(len(nums)):                      # for every possible starting num
+        l: List[int] = [nums[k]]                    # l is a List[int] containing the starting num
+        w: List[int] = nums[:k] + nums[k + 1:]      # w is a List[int] containing nums minus the starting num
+        for p in permute(w):                        # every possible permutations after the first num
+            answer.append(l + p)                    # are appended to answer List[List[int]]
 
     return answer
