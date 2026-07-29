7019c98b62395d2ec22e1fec650807236fbedb44
mercierd575 <mercierd575@gmail.com>
2026-07-29 15:56:46 -0400

    Added all the LeetCode examples as unit tests

--- LeetCode/46 - Permutations/permutationsTest.py
+++ LeetCode/46 - Permutations/permutationsTest.py
@@ -11,6 +11,18 @@ class MyTestCase(unittest.TestCase):
         expected: List[List[int]] = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
         self.assertEqual(expected, perm)
 
+    def test_case_2(self):
+        i: List[int] = [0,1]
+        perm: List[List[int]] = permute(i)
+        expected: List[List[int]] = [[0, 1], [1, 0]]
+        self.assertEqual(expected, perm)
+
+    def test_case_3(self):
+        i: List[int] = [1]
+        perm: List[List[int]] = permute(i)
+        expected: List[List[int]] = [[1]]
+        self.assertEqual(expected, perm)
+
 
 if __name__ == '__main__':
     unittest.main()
