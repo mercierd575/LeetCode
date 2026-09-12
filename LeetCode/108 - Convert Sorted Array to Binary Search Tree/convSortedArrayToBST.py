aca5efd21e0189493c812dcd6b21edc73236ecc8
mercierd575 <mercierd575@gmail.com>
2026-09-12 19:41:44 -0400

    Fixed unit testing and solved problem

--- LeetCode/108 - Convert Sorted Array to Binary Search Tree/convSortedArrayToBST.py
+++ LeetCode/108 - Convert Sorted Array to Binary Search Tree/convSortedArrayToBST.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, Optional
+
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -7,7 +8,20 @@ class TreeNode:
         self.left = left
         self.right = right
 
+    def __eq__(self, other):
+        if not isinstance(other, TreeNode):
+            return NotImplemented
+        return self.val == other.val and self.left == other.left and self.right == other.right
 
 
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if len(nums) < 1:
+            return None
+
+        mid = len(nums) // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid + 1:])
+
+        return root
--- LeetCode/108 - Convert Sorted Array to Binary Search Tree/convSortedArrayToBSTTest.py
+++ LeetCode/108 - Convert Sorted Array to Binary Search Tree/convSortedArrayToBSTTest.py
@@ -6,10 +6,12 @@ class MyTestCase(unittest.TestCase):
     def test_1(self):
         input_1 = [-10, -3, 0, 5, 9]
         output_1: TreeNode = TreeNode(0,
-                                      TreeNode(-3, -10, None),
-                                      TreeNode(9, 5))
+                                      TreeNode(-3, TreeNode(-10), None),
+                                      TreeNode(9, TreeNode(5)))
 
-        self.assertEqual(output_1, Solution.sortedArrayToBST(input_1))
+        sol = Solution()
+
+        self.assertEqual(output_1, sol.sortedArrayToBST(input_1))
 
 
 if __name__ == '__main__':
